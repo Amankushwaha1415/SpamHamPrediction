37926118193fbe1a9e556e3c89e510e5effc4f67
Amankushwaha1415 <amankushwaha141523@gmail.com>
2025-06-29 15:36:33 +0530

    correct the mistake2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import re
 import nltk
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+
 
 
 ps=PorterStemmer()
@@ -213,7 +213,7 @@ def text_preprocessing(text):
     text=re.sub(r'[^\w\s]', '', text) # remove punctuation
     text=re.sub(r'\s+', ' ', text) # remove extra spaces
     text=text.strip() # remove leading and trailing spaces
-    text=word_tokenize(text) #tokenize the word
+    text = text.strip().split() #tokenize the word
     text=[ps.stem(word) for word in text if word not in stopword]  # remove stop words and stem the words
     # print (text) # print the preprocessed text
     text=' '.join(text)
